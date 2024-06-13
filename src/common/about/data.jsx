1ae3dec07538f52e6b2df5e540fb47b1cee44b1a
umakant3525 <umakant3525@gmail.com>
2024-06-13 11:33:07 +0530

    about pages changes

--- src/common/about/data.jsx
+++ src/common/about/data.jsx
@@ -2,25 +2,25 @@ export default [
   {
     id: 1,
     img: require('../../../assets/about/1.png'),
-    title: 'Effortless Maintenance with PVProtect',
-    description: 'Experience seamless daily updates and efficient asset management for your solar plants with PVProtect. Enhance technician productivity and keep users informed with real-time data.'
+    title: 'Easy Solar Maintenance',
+    description: 'Get daily updates and manage your solar plants easily with PVProtect. Perfect for technicians and users.'
   },
   {
     id: 2,
     img: require('../../../assets/about/2.png'),
-    title: 'Reliable Plant Information at Your Fingertips',
-    description: 'Stay connected and up-to-date with the latest information about your solar plants. PVProtect ensures you have all the necessary details right when you need them.'
+    title: 'Stay Informed',
+    description: 'Keep up with the latest information about your solar plants with PVProtect. Always know what’s happening.'
   },
   {
     id: 3,
     img: require('../../../assets/about/3.png'),
-    title: 'Lifetime Support with PVProtect',
-    description: 'We provide unparalleled lifetime support to ensure your solar assets are always protected and functioning at their best. Trust PVProtect to be your constant companion in solar asset management.'
+    title: 'Support You Can Trust',
+    description: 'PVProtect offers reliable support for your solar plants. We are here to help anytime.'
   },
   {
     id: 4,
     img: require('../../../assets/about/4.png'),
-    title: 'Choose Excellence with PVProtect',
-    description: 'Opt for the best in solar plant management. PVProtect offers top-notch features to streamline your operations, making it the ultimate choice for both technicians and users.'
+    title: 'Choose the Best',
+    description: 'PVProtect makes managing solar plants easy and efficient. The best choice for both technicians and users.'
   }
 ];
